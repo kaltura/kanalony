9d3d884391791e58a7538f406307a797095f2eb9
elad.benedict <elad.benedict@kaltura.com>
2016-02-16 16:09:42 +0200

    Fix generated code indentation

--- kanalony-storage-access/src/main/scala/kanalony/storage/generated/hourly_user_activity_clst_entryRow.scala
+++ kanalony-storage-access/src/main/scala/kanalony/storage/generated/hourly_user_activity_clst_entryRow.scala
@@ -2,8 +2,8 @@ package kanalony.storage.generated
 
 import org.joda.time.DateTime
 case class hourly_user_activity_clst_entryRow(partner_id:Int,
-event_type:Int,
-year:Int,
-hour:DateTime,
-entry_id:String,
-count:Long)
+                                              event_type:Int,
+                                              year:Int,
+                                              hour:DateTime,
+                                              entry_id:String,
+                                              count:Long)
