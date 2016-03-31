771a53afafe615f1a05ad99fab40bcf870b668be
elad.benedict <elad.benedict@kaltura.com>
2016-03-31 10:20:16 +0300

    Update scheduling + some cosmetics

--- kanalony-web-server/test/load/LoadTest.scala
+++ kanalony-web-server/test/load/LoadTest.scala
@@ -1,35 +1,45 @@
+package kanalony.tests.load
+
 /**
  * Created by elad.benedict on 3/30/2016.
  */
 
-package computerdatabase // 1
-
-import io.gatling.core.Predef._ // 2
+import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class BasicSimulation123 extends Simulation { // 3
+class DataApiLoadTest extends Simulation {
 
-  val httpConf = http // 4
-    .contentTypeHeader("application/json") // 6
+  val webServerHostname = "il-bigdata-4.dev.kaltura.com"
+  val queryUrl = s"http://${webServerHostname}:9000/query"
+  val queryData = """{"from":"1","to":"1487265988000","filters":[{"dimension":"partner","values":["1"]},{"dimension":"entry","values":["1_Entry_1","1_Entry_5"]}],"dimensions":["day","partner","country"],"metrics":["play"]}"""
+
+  val httpConf = http
+    .contentTypeHeader("application/json")
     .doNotTrackHeader("1")
 
-  val scn = scenario("BasicSimulation").exec( // 7
-    http("request_1")  // 8
-    .post("http://il-bigdata-4.dev.kaltura.com:9000/query")
-    .body(StringBody("""{"from":"1","to":"1487265988000","filters":[{"dimension":"partner","values":["1"]},{"dimension":"entry","values":["1_Entry_1","1_Entry_5"]}],"dimensions":["day","partner","country"],"metrics":["play"]}"""))
-    .check(bodyString.saveAs("myresponse"))
+  val scn = scenario("DataApiLoadTest").exec(
+    http("BasicQuery")
+      .post(queryUrl)
+      .body(StringBody(queryData))
+      //.check(bodyString.saveAs("responseData"))
   ).exec(session => {
-    val maybeId = session.get("myresponse").asOption[String]
-    println(maybeId.getOrElse("ERROR GETTING RESPONSE DATA"))
+    //val ResBody = session.get("responseData").asOption[String]
+    //println(maybeId.getOrElse("ERROR GETTING RESPONSE DATA"))
     session
   })
 
-
-  setUp( // 11
+  setUp(
     scn.inject(
-      atOnceUsers(1),
-      nothingFor(1 seconds),
-      constantUsersPerSec(500) during(15 seconds) // 4) // 12
-  ).protocols(httpConf)) // 13
+      constantUsersPerSec(100) during (30 seconds),
+      constantUsersPerSec(200) during (30 seconds),
+      constantUsersPerSec(300) during (30 seconds),
+      constantUsersPerSec(400) during (30 seconds),
+      constantUsersPerSec(500) during (30 seconds),
+      constantUsersPerSec(600) during (30 seconds),
+      constantUsersPerSec(700) during (30 seconds),
+      constantUsersPerSec(800) during (30 seconds),
+      constantUsersPerSec(900) during (30 seconds),
+      constantUsersPerSec(1000) during (30 seconds)
+    ).protocols(httpConf))
 }
