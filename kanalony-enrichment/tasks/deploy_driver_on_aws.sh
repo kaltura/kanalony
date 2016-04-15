f7c2957d93a496dff952ceb79cb67208a3643065
Ofir Kerker <ofir.kerker@kaltura.com>
2016-04-15 21:29:26 +0300

    Add deploy script for AWS

---
+++ kanalony-enrichment/tasks/deploy_driver_on_aws.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+scp /Users/ofirk/projects/kaltura/kanalony/kanalony-enrichment/target/scala-2.11/kanalony-enrichment.jar pa-front-stg1:~/kanalony/.;
+ssh pa-front-stg1 '~/kanalony/scripts/deploy-full-enrichment.sh'
