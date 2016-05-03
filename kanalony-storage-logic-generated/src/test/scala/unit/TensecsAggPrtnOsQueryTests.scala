999414ae01c9281f4a3972cde1317f96b79e37a3
elad.benedict <elad.benedict@kaltura.com>
2016-05-03 16:01:25 +0300

    Additional fixes

--- kanalony-storage-logic-generated/src/test/scala/unit/TensecsAggPrtnOsQueryTests.scala
+++ kanalony-storage-logic-generated/src/test/scala/unit/TensecsAggPrtnOsQueryTests.scala
@@ -5,7 +5,7 @@ import kanalony.storage.generated.{TensecsAggPrtnOsRow, ITensecsAggPrtnOsTableAc
 import kanalony.storage.logic.generated.{TensecsAggPrtnOsQuery}
 import kanalony.storage.logic.queries.model.{DimensionEqualityConstraint, DimensionUnconstrained, QueryDimensionDefinition}
 import kanalony.storage.logic.{Dimensions, QueryParams, QueryResult}
-import org.joda.time.{LocalDateTime, DateTime}
+import org.joda.time.{DateTimeZone, LocalDateTime, DateTime}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
@@ -37,8 +37,8 @@ class TensecsAggPrtnOsQueryTests extends FunSpec with MockFactory with BeforeAnd
         Dimensions.partner,
         new DimensionEqualityConstraint[Int](Set(1, 2)), true)),
         List(Metrics.play),
-        new LocalDateTime(1),
-        new LocalDateTime(1000))
+        new LocalDateTime(1, DateTimeZone.UTC),
+        new LocalDateTime(1000, DateTimeZone.UTC))
 
       whenReady(query.query(params).failed){ e =>
         e shouldBe a [IllegalArgumentException]
@@ -46,30 +46,30 @@ class TensecsAggPrtnOsQueryTests extends FunSpec with MockFactory with BeforeAnd
     })
 
     it("Should query table with the correct parameters")({
-      configureStub(List(1,2),List(1,2),List("play"),List(19700101),new DateTime(1), new DateTime(1000),
-        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1),5)))
+      configureStub(List(1,2),List(1,2),List("play"),List(19700101),new DateTime(1, DateTimeZone.UTC), new DateTime(1000, DateTimeZone.UTC),
+        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1, DateTimeZone.UTC),5)))
 
       val params = QueryParams(List(
         createPartnerDimensionDefintion(Set(1, 2)),
         createOsDimensionDefintion(Set(1, 2))),
         List(Metrics.play),
-        new LocalDateTime(1),
-        new LocalDateTime(1000))
+        new LocalDateTime(1, DateTimeZone.UTC),
+        new LocalDateTime(1000, DateTimeZone.UTC))
 
       whenReady(query.query(params)){ res => assert(res == List(QueryResult(List("partner","operatingSystem","play"),List(List("1","1","5.0"))))) }
     })
 
     it("Should aggregate correctly when there are no group by columns (i.e. cross-partner)")({
-      configureStub(List(1,2),List(1,2),List("play"),List(19700101),new DateTime(1), new DateTime(1000),
-        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1),5),
-             TensecsAggPrtnOsRow(2,1,"play",19700101, new DateTime(1),1)))
+      configureStub(List(1,2),List(1,2),List("play"),List(19700101),new DateTime(1, DateTimeZone.UTC), new DateTime(1000, DateTimeZone.UTC),
+        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1, DateTimeZone.UTC),5),
+             TensecsAggPrtnOsRow(2,1,"play",19700101, new DateTime(1, DateTimeZone.UTC),1)))
 
       val params = QueryParams(List(
         createPartnerDimensionDefintion(Set(1, 2), false),
         createOsDimensionDefintion(Set(1, 2), false)),
         List(Metrics.play),
-        new LocalDateTime(1),
-        new LocalDateTime(1000))
+        new LocalDateTime(1, DateTimeZone.UTC),
+        new LocalDateTime(1000, DateTimeZone.UTC))
 
       whenReady(query.query(params)){ res => assert(res == List(QueryResult(List("play"),List(List("6.0"))))) }
     })
@@ -83,35 +83,35 @@ class TensecsAggPrtnOsQueryTests extends FunSpec with MockFactory with BeforeAnd
     })
 
     it("Should aggregate correctly over several metrics")({
-      configureStub(List(1),List(1),List("play","playerImpression"),List(19700101),new DateTime(1), new DateTime(1000),
-        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1).plusMinutes(5),1),
-          TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1).plusMinutes(5),2),
-          TensecsAggPrtnOsRow(1,1,"playerImpression",19700101, new DateTime(1),3),
-          TensecsAggPrtnOsRow(1,1,"playerImpression",19700101, new DateTime(1).plusHours(1),4)))
+      configureStub(List(1),List(1),List("play","playerImpression"),List(19700101),new DateTime(1, DateTimeZone.UTC), new DateTime(1000, DateTimeZone.UTC),
+        List(TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1, DateTimeZone.UTC).plusMinutes(5),1),
+          TensecsAggPrtnOsRow(1,1,"play",19700101, new DateTime(1, DateTimeZone.UTC).plusMinutes(5),2),
+          TensecsAggPrtnOsRow(1,1,"playerImpression",19700101, new DateTime(1, DateTimeZone.UTC),3),
+          TensecsAggPrtnOsRow(1,1,"playerImpression",19700101, new DateTime(1, DateTimeZone.UTC).plusHours(1),4)))
 
       val params = QueryParams(List(
         createPartnerDimensionDefintion(Set(1)),
         createOsDimensionDefintion(Set(1))),
         List(Metrics.play, Metrics.playerImpression),
-        new LocalDateTime(1),
-        new LocalDateTime(1000))
+        new LocalDateTime(1, DateTimeZone.UTC),
+        new LocalDateTime(1000, DateTimeZone.UTC))
 
       whenReady(query.query(params)){ res => assert(res == List(QueryResult(List("partner", "operatingSystem", "play"),List(List("1", "1", "3.0"))), QueryResult(List("partner", "operatingSystem", "playerImpression"),List(List("1", "1", "7.0"))))) }
     })
 
     it("Should aggregate correctly over max-logic metric")({
-      configureStub(List(1),List(1),List("peakView"),List(19700101),new DateTime(1), new DateTime(1000),
-        List(TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1).plusMinutes(1),1),
-          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1).plusMinutes(2),200),
-          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1).plusMinutes(3),3),
-          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1).plusHours(4),4)))
+      configureStub(List(1),List(1),List("peakView"),List(19700101),new DateTime(1, DateTimeZone.UTC), new DateTime(1000, DateTimeZone.UTC),
+        List(TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1, DateTimeZone.UTC).plusMinutes(1),1),
+          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1, DateTimeZone.UTC).plusMinutes(2),200),
+          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1, DateTimeZone.UTC).plusMinutes(3),3),
+          TensecsAggPrtnOsRow(1,1,"peakView",19700101, new DateTime(1, DateTimeZone.UTC).plusHours(4),4)))
 
       val params = QueryParams(List(
         createPartnerDimensionDefintion(Set(1)),
         createOsDimensionDefintion(Set(1))),
         List(Metrics.peakView),
-        new LocalDateTime(1),
-        new LocalDateTime(1000))
+        new LocalDateTime(1, DateTimeZone.UTC),
+        new LocalDateTime(1000, DateTimeZone.UTC))
 
       whenReady(query.query(params)){ res => assert(res == List(QueryResult(List("partner", "operatingSystem", "peakView"),List(List("1", "1", "200.0"))))) }
     })
@@ -119,8 +119,8 @@ class TensecsAggPrtnOsQueryTests extends FunSpec with MockFactory with BeforeAnd
 
     it("Should query table with the correct parameters when time spans more than a single day")({
 
-      val start = new DateTime(2015,12,31,1,1)
-      val end = new DateTime(2016,1,1,1,1)
+      val start = new DateTime(2015,12,31,1,1, DateTimeZone.UTC)
+      val end = new DateTime(2016,1,1,1,1, DateTimeZone.UTC)
 
       configureStub(List(1,2),List(1,2),List("play"),List(20151231,20160101),start, end,
         List(TensecsAggPrtnOsRow(1,1,"play",20151231, start.plusHours(3), 5),
@@ -138,8 +138,8 @@ class TensecsAggPrtnOsQueryTests extends FunSpec with MockFactory with BeforeAnd
 
     it("Should aggregate over columns in partition key and clustering key")({
 
-      val start = new DateTime(2015,12,31,1,1)
-      val end = new DateTime(2016,1,1,1,1)
+      val start = new DateTime(2015,12,31,1,1, DateTimeZone.UTC)
+      val end = new DateTime(2016,1,1,1,1, DateTimeZone.UTC)
 
       configureStub(List(1),List(1,2),List("play"),List(20151231,20160101),start, end,
         List(TensecsAggPrtnOsRow(1,1,"play",20151231, start.plusHours(3),5),
