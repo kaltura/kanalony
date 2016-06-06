91d78692140ff597a570138e76f1d35fd63b53e5
Ofir Kerker <ofir.kerker@kaltura.com>
2016-06-06 14:26:21 +0300

    Fix deploy script

--- kanalony-enrichment/tasks/deploy_driver_on_aws.sh
+++ kanalony-enrichment/tasks/deploy_driver_on_aws.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 scp /Users/ofirk/projects/kaltura/kanalony/kanalony-enrichment/target/scala-2.11/kanalony-enrichment.jar pa-front-stg1:~/kanalony/.;
-ssh pa-front-stg1 '~/kanalony/scripts/deploy-full-enrichment.sh'
+ssh pa-front-stg1 '~/kanalony/scripts/deploy-full-enrichment-prod.sh'
