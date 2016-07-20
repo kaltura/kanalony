8c9d636a252c033fd640d4e98ac41b58d9d554b3
Orly Lampert <orly.lampert@kaltura.com>
2016-07-20 16:21:28 +0300

    Use view metric instead of tenSecsViewed to calculate estimated time viewed

--- kanalony-storage-logic/src/main/scala/kanalony/storage/logic/queries/EstimatedMinutesWatchedQuery.scala
+++ kanalony-storage-logic/src/main/scala/kanalony/storage/logic/queries/EstimatedMinutesWatchedQuery.scala
@@ -2,16 +2,13 @@ package kanalony.storage.logic.queries
 
 import com.kaltura.model.entities.{Metric, Metrics}
 import kanalony.storage.logic._
-import kanalony.storage.logic.queries.model.IDimensionDefinition
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 /**
  * Created by elad.benedict on 3/14/2016.
  */
 
 class EstimatedMinutesWatchedQuery(queryParams: QueryParams, queryLocator: IQueryLocator) extends ComputedQuery(Metrics.estimatedMinutesWatched, queryParams, queryLocator) {
-  override val requiredMetrics: List[Metric] = List(Metrics.tenSecsViewed)
+  override val requiredMetrics: List[Metric] = List(Metrics.view)
 
   override def computeValue(groupMetricsValues: List[SingleMetricValue]): Double = {
     groupMetricsValues.head.value / 6
